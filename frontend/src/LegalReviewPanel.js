f560902c76fa82750aca7852722205fbca3e3d53
sudheer-gentela <sudheer.gentela@gmail.com>
2026-03-07 00:18:23 +0530

    Adding CLM module 2

--- frontend/src/LegalReviewPanel.js
+++ frontend/src/LegalReviewPanel.js
@@ -1,5 +1,5 @@
 // LegalReviewPanel.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { apiService } from './apiService';
 
 function fmt(dt) {
